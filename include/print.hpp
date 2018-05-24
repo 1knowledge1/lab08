b438f350d7c1a663b722d01f1ee6126ba1d2da4f
1knowledge1 <iaroslaw.bondarenko@yandex.ru>
2018-05-24 21:49:12 +0300

    added doxygen.conf

--- include/print.hpp
+++ include/print.hpp
@@ -1,6 +1,11 @@
 #include <string>
 #include <fstream>
 #include <iostream>
-
+/** @brief  Вывод строки на экран
+    @param  Принимает ссылку на строку text и ссылку на поток вывода out
+*/
 void print(const std::string& text, std::ostream& out = std::cout);
+/** @brief  Вывод строки в файл
+    @param  Принимает на вход ссылку на строку text,и ссылку на поток вывода в файл out
+*/
 void print(const std::string& text, std::ofstream& out);
